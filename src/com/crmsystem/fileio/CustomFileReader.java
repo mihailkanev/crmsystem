7b6a05bbab76e0be85d3dab20aaa9c082618ff2b
Your Name <you@example.com>
2023-12-04 12:09:41 +0200

    Update CustomFileReader.java

--- src/com/crmsystem/fileio/CustomFileReader.java
+++ src/com/crmsystem/fileio/CustomFileReader.java
@@ -4,8 +4,7 @@ import com.crmsystem.models.Client;
 
 import java.util.Map;
 
-public class CustomFileReader {
-    public Map<Integer, Client> readData() {
-        return null;
-    }
+public interface CustomFileReader {
+   Map<Integer, Client> readData();
+
 }
