c334d91ba46bad32a08c871a899a03de26f48266
Your Name <you@example.com>
2023-12-05 11:29:58 +0200

    Update CsvDataFile.java with opencsv library

--- src/com/crmsystem/fileio/CsvDataFile.java
+++ src/com/crmsystem/fileio/CsvDataFile.java
@@ -1,17 +1,43 @@
 package com.crmsystem.fileio;
 
 import com.crmsystem.models.Client;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
-import java.util.Map;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
-public class CsvDataFile implements CustomFileWriter, CustomFileReader{
+public class CsvDataFile implements CustomFileWriter, CustomFileReader {
     private String filePath;
-    public CsvDataFile(String filePath){
+
+    public CsvDataFile(String filePath) {
         this.filePath = filePath;
     }
+
     @Override
     public Map<Integer, Client> readData() {
-        return null;
+        Map<Integer, Client> clientMap = new HashMap<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            List<String[]> records = reader.readAll();
+
+            for (int i = 1; i < records.size(); i++) {
+                String[] fields = records.get(i);
+
+                int id = Integer.parseInt(fields[0].trim());
+                String name = fields[1].trim();
+                String industry = fields[2].trim();
+                String contactPerson = fields[3].trim();
+                double revenue = Double.parseDouble(fields[4].trim());
+
+                Client client = new Client(id, name, industry, contactPerson, revenue);
+                clientMap.put(id, client);
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return clientMap;
     }
 
     @Override
