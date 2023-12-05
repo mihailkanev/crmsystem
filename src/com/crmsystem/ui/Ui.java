73f60cb9fb50e479351d94ab10b6572aa3f014a0
Your Name <you@example.com>
2023-12-05 11:37:50 +0200

    Add user interface options to Ui.java

--- src/com/crmsystem/ui/Ui.java
+++ src/com/crmsystem/ui/Ui.java
@@ -1,7 +1,34 @@
 package com.crmsystem.ui;
 
+import com.crmsystem.models.ClientService;
+import com.crmsystem.models.Service;
+
 public class Ui {
-    public String displayOptions(){
-        return null;
+    private Service clientService;
+
+    public void displayOptions() {
+
+        System.out.println("Choose menu option:");
+        System.out.println("1. Add client");
+        System.out.println("2. Edit client");
+        System.out.println("3. View clients");
+    }
+
+    public void handleUserInput(int option) {
+        switch (option) {
+            case 1:
+                // Add client logic
+                break;
+            case 2:
+                // Edit client logic
+                break;
+            case 3:
+                // View clients logic
+                clientService.viewClients();
+                break;
+            // handle other options...
+            default:
+                System.out.println("Invalid option");
+        }
     }
 }
