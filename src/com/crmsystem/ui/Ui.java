d720aa835705b781e2ee22ed7c663b8b7761bf7c
Your Name <you@example.com>
2023-12-11 18:23:36 +0200

    adding menu for UI

--- src/com/crmsystem/ui/Ui.java
+++ src/com/crmsystem/ui/Ui.java
@@ -1,10 +1,16 @@
 package com.crmsystem.ui;
 
-import com.crmsystem.models.ClientService;
+import com.crmsystem.models.Client;
 import com.crmsystem.models.Service;
 
+import java.util.Scanner;
+
 public class Ui {
-    private Service clientService;
+    private Scanner scanner;
+
+    public Ui(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
     public void displayOptions() {
 
@@ -16,34 +22,5 @@ public class Ui {
         System.out.println("5. Search by name");
         System.out.println("6. Remove by ID");
         System.out.println("7. Save and Exit");
-    }
-
-    public void handleUserInput(int option) {
-        switch (option) {
-            case 1:
-                // Add client logic
-                break;
-            case 2:
-                // Edit client logic
-                break;
-            case 3:
-                // View clients logic
-                clientService.viewClients();
-                break;
-            case 4:
-                // Search by Id
-                break;
-            case 5:
-                //Search by Name
-                break;
-            case 6:
-                // Remove by id
-                break;
-            case 7:
-                // save and exit
-                break;
-            default:
-                System.out.println("Invalid option");
-        }
     }
 }
