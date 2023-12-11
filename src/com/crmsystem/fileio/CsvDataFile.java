d2e8cf0497eb51a8dd1798e732b3dcc9f5df2e93
Your Name <you@example.com>
2023-12-11 18:13:51 +0200

    Add Apache Commons CSV

--- src/com/crmsystem/fileio/CsvDataFile.java
+++ src/com/crmsystem/fileio/CsvDataFile.java
@@ -2,10 +2,17 @@ package com.crmsystem.fileio;
 
 import com.crmsystem.models.Client;
 import com.opencsv.CSVReader;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVFormat;
+
 import com.opencsv.exceptions.CsvException;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.*;
 
 public class CsvDataFile implements CustomFileWriter, CustomFileReader {
@@ -19,29 +26,45 @@ public class CsvDataFile implements CustomFileWriter, CustomFileReader {
     public Map<Integer, Client> readData() {
         Map<Integer, Client> clientMap = new HashMap<>();
 
-        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
-            List<String[]> records = reader.readAll();
+        try (Reader reader = new FileReader(filePath);
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader())) {
 
-            for (int i = 1; i < records.size(); i++) {
-                String[] fields = records.get(i);
-
-                int id = Integer.parseInt(fields[0].trim());
-                String name = fields[1].trim();
-                String industry = fields[2].trim();
-                String contactPerson = fields[3].trim();
-                double revenue = Double.parseDouble(fields[4].trim());
+            for (CSVRecord csvRecord : csvParser) {
+                int id = Integer.parseInt(csvRecord.get("ID"));
+                String name = csvRecord.get("Name");
+                String industry = csvRecord.get("Industry");
+                String contactPerson = csvRecord.get("ContactPerson");
+                double revenue = Double.parseDouble(csvRecord.get("Revenue"));
 
                 Client client = new Client(id, name, industry, contactPerson, revenue);
                 clientMap.put(id, client);
             }
-        } catch (IOException | CsvException e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
         return clientMap;
     }
 
     @Override
     public void writeData(Map<Integer, Client> clientMap) {
+        try (CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(filePath), CSVFormat.DEFAULT
+                .withHeader("ID", "Name", "Industry", "ContactPerson", "Revenue"))) {
 
+            for (Client client : clientMap.values()) {
+                csvPrinter.printRecord(
+                        client.getId(),             // ID as int
+                        client.getName(),
+                        client.getIndustry(),
+                        client.getContactPerson(),
+                        client.getRevenue()          // Assuming client.getRevenue() returns a double
+                );
+            }
+
+            System.out.println("Data written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
